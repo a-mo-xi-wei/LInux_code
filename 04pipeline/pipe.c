08f0a0125adb9b2cfe8ec8e46e89c3b59fe958d0
WeiWang <1428206861@qq.com>
2024-09-14 15:24:24 +0800

    管道操作

---
+++ 04pipeline/pipe.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+    > File Name: pipe.c
+    > Author: 威桑
+    > Mail: 1428206861@qq.com 
+    > Created Time: 2024年09月13日 星期五 17时13分07秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<pthread.h>
+void syserr(char* str){
+	perror(str);
+	exit(1);
+}
+int main(int argc,char* argv[]){
+	
+	return 0;
+}
